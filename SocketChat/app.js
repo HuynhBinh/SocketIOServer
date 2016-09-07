cac4149c2ecbff87b28a9569c34ee27a38c529d2
Binh Huynh <huynhbinh0511@gmail.com>
2016-09-07 17:37:04 +0700

    commit

--- SocketChat/app.js
+++ SocketChat/app.js
@@ -2,8 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-
-server.listen(3100);
+server.listen(8080);
 server.on('error', onError);
 server.on('listening', onListening);
 
@@ -15,37 +14,48 @@ app.get('/', function(req, res)
 });
 
 
-
 io.on('connection', function (socket)
 {
-
-    console.log('user connect');
-
+    console.log('new user connected');
 
     io.emit('join', 'user ' + socket.id + ' has joined');
 
-
     socket.on('chat message', function(msg)
     {
-      //console.log('message: ' + msg);
 
-      socket.broadcast.emit('broadcast', msg);
+        // send message to all the socket connected except the sender
+        //socket.broadcast.emit('broadcast', msg);
+
+
+        // send message to all the socket connected
+        //io.emit('broadcast', msg);
+
+
+        //process data before send back to client
+        // send message to a specific socket id
+        io.to(socket.id).emit('broadcast', processMessage(msg));
+
+    });
+
+
+    socket.on('from android', function(msg)
+    {
+        io.emit('to android', 'Echo from server' +  msg);
 
     });
 
 
     socket.on('typing', function()
     {
-      //console.log('user is typing');
-
-      socket.broadcast.emit('usertyping', 'user is typing');
+        //console.log('user is typing');
+        socket.broadcast.emit('usertyping', 'user is typing');
 
     });
 
 
     socket.on('disconnect', function()
     {
-      console.log('user disconnected');
+      console.log('a user disconnected');
     });
 
 });
@@ -54,7 +64,10 @@ io.on('connection', function (socket)
 
 
 
-
+function processMessage(message)
+{
+    return 'From server: ' + message;
+}
 
 
 
@@ -69,7 +82,7 @@ function onError(error)
       ? 'Pipe ' + port
       : 'Port ' + port;
 
-  // handle specific listen errors with friendly messages
+
   switch (error.code)
   {
     case 'EACCES':
@@ -93,4 +106,5 @@ function onListening()
       : 'port ' + addr.port;
 
 }
+
 module.exports = app;
